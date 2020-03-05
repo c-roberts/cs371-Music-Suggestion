482cf852a0eefc27365008d13a07ae574779d019
c-roberts <36252679+c-roberts@users.noreply.github.com>
2020-03-05 16:06:28 -0600

    kb changes

--- parser.py
+++ parser.py
@@ -20,7 +20,7 @@ for s in song_list:
         continue
     else: 
         # write stuff to file as ISAs
-        s_title = s['title'].replace(" ", "")
+        s_title = s['title'].replace(" ", "").replace("'", "")
         s_artist = s['artist'].replace(" ", "")
 
         f.write("\n(isa {} Song-CW)\n".format(s_title))
@@ -32,7 +32,8 @@ for s in song_list:
 
         # dances
         for d in s['dances']:
-            f.write("(CanBeDancedTo {} {})\n".format(d.replace(" ", ""), s_title))
+            d_i = d.replace(" ", "").replace("(", "/").replace(")", "")
+            f.write("(CanBeDancedTo {} {})\n".format(d_i, s_title))
    
 
         
