5a706a95398d8a8e25d554d7ef4cb47723750b50
c-roberts <36252679+c-roberts@users.noreply.github.com>
2020-03-07 17:59:27 -0600

    updated parser

--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-#from Data_Collection.collect import getSongs
 import json
 import sys
 
@@ -10,42 +9,51 @@ def translateVal(v):
     else:
         return "highL"
 
+def translatePitch(v):
+    if v <= 250:
+        return "lowL"
+    elif v <= 500:
+        return "mediumL"
+    else:
+        return "highL"
+
 ## run scraper -> dictionary
+##from Data_Collection.collect import getSongs
 ##song_list = getSongs()
 
 # open song list from json
-with open(sys.path[0] + '/Data_Collection/data.json') as f:
-    song_list = json.load(f)
+with open(sys.path[0] + '/Data_Collection/data.json') as fj:
+    song_list = json.load(fj)
 
 print("Loaded data from json file.")
 
 # initialize file
-with open("KRR-SongsFacts.krf", "w") as f:
-    f.write("(in-microtheory cs371-Music-Suggestion)\n")
+f = open(sys.path[0] + '/kb/KRR-SongsFacts.krf', "w") 
+f.write("(in-microtheory cs371-Music-Suggestion)\n")
 
-    for s in song_list:
-        s_title = s['title'].replace(" ", "").replace("'", "")
-        s_artist = s['artist'].replace(" ", "")
-        
-        if s['beat'] == None or s['energy'] == None or s['mood'] == None or s['tempo'] == None:
-            print("Song \"{}\" skipped due to missing essential data".format(s_title))
-            continue
-        else: 
-            # write stuff to file as ISAs
-            
-            f.write("\n(isa {} Song-CW)\n".format(s_title))
-            f.write("(composerOfMusicalCW {} {})\n".format(s_artist, s_title))
-            f.write("(TempoOfSong {} {})\n".format(s['tempo'], s_title))
-            f.write("(EnergyOfSong {} {})\n".format(translateVal(s['energy']), s_title))
-            #f.write("(MoodOfSong {} {})\n".format(translateVal(s['mood']), s_title))
-            f.write("(BeatOfSong {} {})\n".format(translateVal(s['beat']), s_title))
-
-            # dances
-            for d in s['dances']:
-                d_i = d.replace(" ", "").replace("(", "/").replace(")", "")
-                f.write("(CanBeDancedTo {} {})\n".format(d_i, s_title))
-        #print("Done with \"{}\".".format(s_title))
-
-    print('Finished parsing {} to knowledge base.'.format(len(song_list)))
+for s in song_list:
+    s_title = s['title'].replace(" ", "").replace("'", "")
+    s_artist = s['artist'].replace(" ", "")
     
+    if s['beat'] == None or s['energy'] == None or s['mood'] == None or s['tempo'] == None:
+        print("Song \"{}\" skipped due to missing essential data".format(s_title))
+        continue
+    else: 
+        # write stuff to file as ISAs
+        f.write("\n(isa {} Song-CW)\n".format(s_title))
+        f.write("(composerOfMusicalCW {} {})\n".format(s_artist, s_title))
+        f.write("(TempoOfSong {} {})\n".format(s['tempo'], s_title))
+        f.write("(EnergyOfSong {} {})\n".format(translateVal(s['energy']), s_title))
+        f.write("(BeatOfSong {} {})\n".format(translateVal(s['beat']), s_title))
+        f.write("(PitchOfSong {} {})\n".format(translatePitch(s['avgfreq']), s_title))
 
+        # dances
+        for d in s['dances']:
+            d_i = d.replace(" ", "").replace("(", "/").replace(")", "")
+            f.write("(CanBeDancedTo {} {})\n".format(d_i, s_title))
+            
+    #print("Done with \"{}\".".format(s_title))
+
+print('Finished parsing {} to knowledge base.'.format(len(song_list)))
+
+f.close()
